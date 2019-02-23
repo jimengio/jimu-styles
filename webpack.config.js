8f0dbcf3e6ac157ca4500003d912b9ec9c19cd54
rebirthO <benyue.liu@jimeng.io>
2019-02-23 14:13:19 +0800

    remove comment

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader" // translates CSS into CommonJS
-        ]
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.tsx?$/,
